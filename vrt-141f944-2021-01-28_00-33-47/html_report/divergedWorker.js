9b53080b4ea6fa6e90416c9a7178410249f074fc
Jian Liao <jianliao@adobe.com>
2021-02-26 22:09:30 -0800

    Monthly cleanup

--- vrt-141f944-2021-01-28_00-33-47/html_report/divergedWorker.js
+++
@@ -1,6 +0,0 @@
-importScripts('pixel.js');
-importScripts('diverged.js');
-self.addEventListener('message', function(e) {
-  self.postMessage(diverged(...e.data.divergedInput));
-  self.close();
-}, false);
