4798546687567c100276fbbae91d3761cfc4c5bf
Travis CI User <travis@example.org>
2021-03-03 14:44:15 +0000

    chore: deploy adobe/spectrum-css VR result to github.com/adobe/spectrum-css-vr-results.git:vrt-fd52a13-2021-03-03_14-44-14

---
+++ vrt-fd52a13-2021-03-03_14-44-14/html_report/divergedWorker.js
@@ -0,0 +1,6 @@
+importScripts('pixel.js');
+importScripts('diverged.js');
+self.addEventListener('message', function(e) {
+  self.postMessage(diverged(...e.data.divergedInput));
+  self.close();
+}, false);
